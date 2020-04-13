439f95acc0c3da34bb8bfcee018fa6b51b259c94
Victor <victorif627@outloo.com>
2020-04-13 17:33:11 -0300

    Altera .gitignore

--- .venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\victo\OneDrive\Área de Trabalho\proje-django\the-message\.venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
