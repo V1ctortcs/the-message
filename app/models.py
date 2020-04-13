123cc823e455afbe6918cb7f6e15c78207a39833
Victor <victorif627@outloo.com>
2020-04-13 19:30:13 -0300

    Implementa Autentificação do login

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    nome = models.CharField(max_length=50)
+    sobrenome = models.CharField(max_length=50)
+    dataNasc = models.DateField
+    phone = models.CharField(max_length=11)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return str(self.id)
+
+    class Meta:
+        db_table = 'users'
--- app/views.py
+++ app/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+
+def login_user(request):
+    return render(request, 'login.html')
+
+@csrf_protect
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password  = request.POST.get('password')
+        print(username)
+        print(password)
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.info(request, 'ACESSOUUUUUU!!!')
+        else:
+            messages.error(request, 'Usuário ou Senha Ivalidos, Tente Novamente')
+    return redirect('/login/')
